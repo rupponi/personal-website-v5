441aa11cdb2af944b5d1351e9bbe5de8de6b0706
rupponi <rponi70@gmail.com>
2019-10-07 00:05:32 -0700

    Updates to color scheme. Experimenting with animations.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,9 +2,21 @@ import React from "react"
 import "./navbar.css"
 
 const NavBar = () => {
+
+    function navBarClick(e) {
+        let title = document.getElementById("name-title");
+
+        title.style.animation = "titleClick";
+        title.style.animationDuration = "0.1s";
+        title.style.animationFillMode = "forwards";
+        title.style.animationTimingFunction = "linear";
+
+        window.scrollTo(0, 0);
+    }
+
     return (
-        <div id = "navbar-container">
-            <h1 id = "name-title">ROHAN UPPONI</h1>
+        <div id = "navbar-container" onClick = {navBarClick}>
+            <h1 id = "name-title" unselectable = "on">ROHAN UPPONI</h1>
         </div>
     );
 }
