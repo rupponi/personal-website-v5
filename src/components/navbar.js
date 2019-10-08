20d42188a1380adf0477ff9285963c3c2ece3f6e
rupponi <rponi70@gmail.com>
2019-10-08 00:43:38 -0700

    Update:
     - Fixed transition refresh of NavBartitle press.
    
     - Added basic menu symbol to use in future implementation of menu.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,23 +4,24 @@ import "./navbar.css"
 function navBarClick(e) {
     let title = document.getElementById("name-title");
 
-    title.style.animation = "titleClick";
-    title.style.animationDuration = "0.1s";
-    title.style.animationFillMode = "forwards";
-    title.style.animationTimingFunction = "linear";
+    title.classList.add("animate-title-click");
 
     window.scrollTo(0, 0);
+
+    title.addEventListener('animationend', () => {
+        title.classList.remove("animate-title-click");
+    });
 }
 
 const NavBar = () => {
 
     return (
         <div id = "navbar-container">
-            <div id = "title-panel">
-                <h1 id = "name-title" onClick = {navBarClick} unselectable = "on">ROHAN UPPONI</h1>
+            <div id = "title-panel" onClick = {navBarClick}>
+                <h1 id = "name-title" unselectable = "on">ROHAN UPPONI</h1>
             </div>
             <div id = "menu-panel">
-                <h1>{'\u2630'}</h1>
+                <h1 id = "menu-symbol" style= {{margin: 0 + 'em'}}>{'\u2630'}</h1>
             </div>
         </div>
     );
