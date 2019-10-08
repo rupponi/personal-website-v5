f83f7dfe216c7bbbf2df39f536b451764d317a18
rupponi <rponi70@gmail.com>
2019-10-07 23:03:20 -0700

    Update:
     - Experimenting with GraphQL and refactored navbar.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,22 +1,27 @@
 import React from "react"
 import "./navbar.css"
 
+function navBarClick(e) {
+    let title = document.getElementById("name-title");
+
+    title.style.animation = "titleClick";
+    title.style.animationDuration = "0.1s";
+    title.style.animationFillMode = "forwards";
+    title.style.animationTimingFunction = "linear";
+
+    window.scrollTo(0, 0);
+}
+
 const NavBar = () => {
 
-    function navBarClick(e) {
-        let title = document.getElementById("name-title");
-
-        title.style.animation = "titleClick";
-        title.style.animationDuration = "0.1s";
-        title.style.animationFillMode = "forwards";
-        title.style.animationTimingFunction = "linear";
-
-        window.scrollTo(0, 0);
-    }
-
     return (
-        <div id = "navbar-container" onClick = {navBarClick}>
-            <h1 id = "name-title" unselectable = "on">ROHAN UPPONI</h1>
+        <div id = "navbar-container">
+            <div id = "title-panel">
+                <h1 id = "name-title" onClick = {navBarClick} unselectable = "on">ROHAN UPPONI</h1>
+            </div>
+            <div id = "menu-panel">
+                <h1>{'\u2630'}</h1>
+            </div>
         </div>
     );
 }
