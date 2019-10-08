472eb65391bfa94818bbd281a1f1c890a5f55d0b
rupponi <rponi70@gmail.com>
2019-10-07 23:03:20 -0700

    Update:
     - Experimenting with GraphQL and refactored navbar.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Rohan Upponi | Software Engineer`,
+    description: `I love to build and build what I love.`,
+    author: `Rohan Upponi`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
 import NavBar from "../components/navbar"
+import { graphql } from "gatsby"
 import "../styles/index.css"
 
-const IndexPage = () => (
+export default ({ data }) => (
   <html>
     <link href = "https://fonts.googleapis.com/css?family=Karla" rel = "stylesheet"></link>
 
@@ -12,19 +12,28 @@ const IndexPage = () => (
       <NavBar/>
     </div>
     <div id = "introduction">
-      <h1 id = "testing-message">This project is currently in the works. You can check out my current portfolio right <a target = "_blank" href = "https://rohanupponi.com">here</a>!</h1>
+      <h1 id = "testing-message">My name is {data.site.siteMetadata.author} and {data.site.siteMetadata.description}</h1>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       <div class="ref-cont">
-        <a target = "_blank" href = "mailto: upponirohan@gmail.com"><i id="mail-ref" class="fa fa-envelope-o"></i></a>
+        <a target = "_blank" rel = "noreferrer noopener" href = "mailto: upponirohan@gmail.com"><i id="mail-ref" class="fa fa-envelope-o"></i></a>
       </div>
       <div class="ref-cont">
-        <a target = "_blank" href = "https://github.com/rupponi"><i id="github-ref" class="fa fa-github"></i></a>
+        <a target = "_blank" rel = "noreferrer noopener" href = "https://github.com/rupponi"><i id="github-ref" class="fa fa-github"></i></a>
       </div>
       <div class="ref-cont">
-        <a target = "_blank" href = "https://www.linkedin.com/in/rohan-upponi-139341108/"><i id="linkedin-ref" class = "fa fa-linkedin"></i></a>
+        <a target = "_blank" rel = "noreferrer noopener" href = "https://www.linkedin.com/in/rohan-upponi-139341108/"><i id="linkedin-ref" class = "fa fa-linkedin"></i></a>
       </div>
     </div>
   </html>
 )
 
-export default IndexPage
+export const query = graphql`
+query {
+    site {
+      siteMetadata {
+        author,
+        description
+      }
+    }
+  }
+`
