c48f31e7f142c663dfde56ba43c02a6b9ed7f796
rupponi <rponi70@gmail.com>
2019-10-08 19:23:58 -0700

    Updates:
     - Updated navbar animation.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,13 +3,26 @@ import "./navbar.css"
 
 function navBarClick(e) {
     let title = document.getElementById("name-title");
+    let container = document.getElementById("navbar-container");
 
     title.classList.add("animate-title-click");
+    container.classList.add("light-navbar");
 
     window.scrollTo(0, 0);
 
     title.addEventListener('animationend', () => {
         title.classList.remove("animate-title-click");
+        container.classList.remove("light-navbar");
+    });
+}
+
+function menuSelect(e) {
+    let menusymbol = document.getElementById("menu-symbol");
+
+    menusymbol.classList.add("animate-title-click");
+
+    menusymbol.addEventListener('animationend', () => {
+        menusymbol.classList.remove("animate-title-click");
     });
 }
 
@@ -20,8 +33,8 @@ const NavBar = () => {
             <div id = "title-panel" onClick = {navBarClick}>
                 <h1 id = "name-title" unselectable = "on">ROHAN UPPONI</h1>
             </div>
-            <div id = "menu-panel">
-                <h1 id = "menu-symbol" style= {{margin: 0 + 'em'}}>{'\u2630'}</h1>
+            <div id = "menu-panel" onClick = {menuSelect}>
+                <h1 id = "menu-symbol" unselectable = "on">{'\u2630'}</h1>
             </div>
         </div>
     );
