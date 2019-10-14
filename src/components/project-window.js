5e3115d29afb90256b22774aeb5d99559e12e5d1
rupponi <upponirohan@gmail.com>
2019-10-14 01:28:20 -0700

    Updates:
     - Added content for about section.
    
     - Added additional animations for scroll navigations.
    
     - Added project-window-component to be used for future development of work section.
    
     - Added blank experience section.

---
+++ src/components/project-window.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./project-window.css"
+
+const ProjectWindow = () => {
+    <div id = "project-window-component">
+        <div id = "project-window-container">
+
+        </div>
+    </div>
+}
+
+export default ProjectWindow;
