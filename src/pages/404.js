70f18a0d94fd9a43a547b3ea7dc71de5deeab818
rupponi <upponirohan@gmail.com>
2019-11-10 00:38:23 -0700

    Updates:
     - Re-added custom 404 page after resolving interference caused by CSS to other pages.
    
     - Minor UI responsiveness updates.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,15 @@
 import React from "react"
+import "../styles/404.css"
 
 const NotFoundPage = () => {
   return (
-    <div id="error-page-container">
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div id="error-message-container">
+      <h1 id = "error-404">404</h1>
+      <h3 id = "page-not-found">Page Not Found</h3>
+
+      <div id = "home-button">
+        <a id = "home-link" rel = "noopener noreferrer" href = "https://rohanupponi.com"><h3 id = "return-home-message">Home</h3></a>
+      </div>
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,45 +4,6 @@ import { Helmet } from "react-helmet"
 import { graphql } from "gatsby"
 import "../styles/index.css"
 
-function mailButtonClick() {
-  let targetButton = document.getElementById("mail-button"), linkOpened = false;
-
-  targetButton.classList.add("animate-ref-button-click");
-  targetButton.addEventListener('animationend', () => {
-    targetButton.classList.remove("animate-ref-button-click");
-    if (!linkOpened) {
-      window.open("mailto: upponirohan@gmail.com");
-      linkOpened = true;
-    }
-  });
-}
-
-function githubButtonClick() {
-  let targetButton = document.getElementById("github-button"), linkOpened = false;
-
-  targetButton.classList.add("animate-ref-button-click");
-  targetButton.addEventListener('animationend', () => {
-    targetButton.classList.remove("animate-ref-button-click");
-    if (!linkOpened) {
-      window.open("https://github.com/rupponi", "_blank");
-      linkOpened = true;
-    }
-  });
-}
-
-function linkedinButtonClick() {
-  let targetButton = document.getElementById("linkedin-button"), linkOpened = false;
-
-  targetButton.classList.add("animate-ref-button-click");
-  targetButton.addEventListener('animationend', () => {
-    targetButton.classList.remove("animate-ref-button-click");
-    if (!linkOpened) {
-      window.open("https://linkedin.com/in/rohanupponi", "_blank");
-      linkOpened = true;
-    }
-  });
-}
-
 export default ({ data }) => (
   <html>
     <link href = "https://fonts.googleapis.com/css?family=Karla" rel = "stylesheet"></link>
@@ -63,9 +24,9 @@ export default ({ data }) => (
       <h1 id = "intro-message">My name is {data.site.siteMetadata.author} and {data.site.siteMetadata.description}</h1>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 
-      <div id = "mail-button" class="ref-cont" onClick = {mailButtonClick}><i id="mail-ref" class="fa fa-envelope-o"></i></div>
-      <div id = "github-button" class="ref-cont" onClick = {githubButtonClick}><i id="github-ref" class="fa fa-github"></i></div>
-      <div id = "linkedin-button" class="ref-cont" onClick = {linkedinButtonClick}><i id="linkedin-ref" class = "fa fa-linkedin"></i></div>
+      <div id = "mail-button" class="ref-cont"><a rel = "noopener noreferrer" target = "_blank" href = "mailto: upponirohan@gmail.com"><i class="ref fa fa-envelope-o"/></a></div>
+      <div id = "github-button" class="ref-cont"><a rel = "noopener noreferrer" target = "_blank" href = "https://github.com/rupponi"><i class="ref fa fa-github"/></a></div>
+      <div id = "linkedin-button" class="ref-cont"><a rel = "noopener noreferrer" target = "_blank" href = "https://linkedin.com/in/rohanupponi"><i class = "ref fa fa-linkedin"/></a></div>
     </div>
 
     <div id = "about">
@@ -262,7 +223,7 @@ export default ({ data }) => (
         </div>
 
         <div id = "signature">
-          <h3>© 2019 Rohan Upponi</h3>
+          <a id = "signature-link" rel = "noopener noreferrer" target = "_blank" href = "https://github.com/rupponi"><h3>© 2019 Rohan Upponi</h3></a>
         </div>
 
         <br/>
