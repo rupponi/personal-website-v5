4c98e234e5a3fc784046f42d8f685daefa7839f6
rupponi <upponirohan@gmail.com>
2019-12-05 20:09:08 -0700

    Update:
     - Re-structured experience card into reusable components for better design and ease-of-use from a scaling perspective.

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -1,34 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import "./experience.css";
+import ExperienceCard from "../experience-card/experience-card";
 
-const Experience = () => {
-    return (
-        <div id = "experience">
-            <h1 class = "section-title">Where I've Worked</h1>
-            <div id = "experience-partition">
-                
-                <div class = "experience-item">
-                    <div class = "title">
-                        <div class = "logo-container">
-                            <span class = "helper"/>
-                            <a rel = "noopener noreferrer" target = "_blank" href = "https://stchealth.com"><img class = "logo" src = "img/stc-logo.png" alt = "STC Work Experience"/></a>
-                        </div>
-                        <div class = "outer-title-container">
-                            <div class = "inner-title-container">
-                                <div class = "experience-title">
-                                    Software Development Intern {" "}
-                                    <a class = "external-links" target = "_blank" rel = "noopener noreferrer" href = "https://stchealth.com/">
-                                        @ Scientific Technologies Corporation
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class = "experience-timeline">
-                        (August 2018 - May 2019)
-                    </div>
-                    <span class = "experience-content-break"/>
-                    <div class = "experience-content">
+let stcExperienceData = {
+    externalLink: 'https://stchealth.com',
+    logoPath: 'img/stc-logo.png',
+    logoDescriptor: 'STC Work Experience',
+    title: 'Software Development Intern',
+    externalLinkText: 'Scientific Technologies Corporation',
+    timeline: '(August 2018 - May 2019)'
+},
+intelExperienceData = {
+    externalLink: 'https://www.intel.com/content/www/us/en/architecture-and-technology/optane-memory.html',
+    logoPath: 'img/intel-logo.png',
+    logoDescriptor: 'Intel Work Experience',
+    title: 'Software Engineering Intern',
+    externalLinkText: 'Intel Corporation',
+    timeline: '(May 2018 - February 2019)'
+},
+ascExperienceData = {
+    externalLink: 'https://www.azcourts.gov/AZSupremeCourt',
+    logoPath: 'img/az-supreme-court-logo.png',
+    logoDescriptor: 'Arizona Supreme Court Work Experience',
+    title: 'Software Development Extern',
+    externalLinkText: 'Arizona Supreme Court',
+    timeline: '(June 2017 - August 2017)'
+},
+asuExperienceData = {
+    externalLink: 'https://cidse.engineering.asu.edu/teaching-research-assistantships-fellowships-awards/',
+    logoPath: 'img/asu-logo.png',
+    logoDescriptor: 'Arizona State University Work Experience',
+    title: 'Undergraduate Teaching Assistant',
+    externalLinkText: 'Arizona State University',
+    timeline: '(January 2017 - May 2017)'
+}
+
+class Experience extends Component {
+    render() {
+        return (
+            <div id = "experience">
+                <h1 class = "section-title">Where I've Worked</h1>
+                <div id = "experience-partition">
+                    
+                    <ExperienceCard experienceData = {stcExperienceData}>
                         <ul class = "experience-description">
                             <li class = "experience-list-item">
                                 Developed API routing backend authentication and account data retrieval services
@@ -43,35 +57,11 @@ const Experience = () => {
                                 potential users.
                             </li>
                         </ul>
-                    </div>
-                </div>
+                    </ExperienceCard>
 
+                    <br/>
 
-                <br/>
-
-
-                <div class = "experience-item">
-                    <div class = "title">
-                        <div class = "logo-container">
-                            <span class = "helper"/>
-                            <a rel = "noopener noreferrer" target = "_blank" href = "https://www.intel.com/content/www/us/en/architecture-and-technology/optane-memory.html"><img class = "logo" src = "img/intel-logo.png" alt = "Intel Work Experience"/></a>
-                        </div>
-                        <div class = "outer-title-container">
-                            <div class = "inner-title-container">
-                                <div class = "experience-title">
-                                    Software Engineering Intern {" "}
-                                    <a class = "external-links" target = "_blank" rel = "noopener noreferrer" href = "https://www.intel.com/content/www/us/en/architecture-and-technology/optane-memory.html">
-                                        @ Intel Corporation
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class = "experience-timeline">
-                        (May 2018 - February 2019)
-                    </div>
-                    <span class = "experience-content-break"/>
-                    <div class = "experience-content">
+                    <ExperienceCard experienceData = {intelExperienceData}>
                         <ul class = "experience-description">
                             <li class = "experience-list-item">
                                 Developed an extensive unit-level testing framework that eliminated
@@ -84,37 +74,11 @@ const Experience = () => {
                                 hardware team on detailed hardware specifications.
                             </li>
                         </ul>
-                    </div>
-                </div>
+                    </ExperienceCard>
 
+                    <br/>
 
-
-                <br/>
-
-
-
-                <div class = "experience-item">
-                    <div class = "title">
-                        <div class = "logo-container">
-                            <span class = "helper"/>
-                            <a rel = "noopener noreferrer" target = "_blank" href = "https://www.azcourts.gov/AZSupremeCourt"><img class = "logo" src = "img/az-supreme-court-logo.png" alt = "AZ Supreme Court Work Experience"/></a>
-                        </div>
-                        <div class = "outer-title-container">
-                            <div class = "inner-title-container">
-                                <div class = "experience-title">
-                                    Software Development Extern {" "}
-                                    <a class = "external-links" target = "_blank" rel = "noopener noreferrer" href = "https://www.azcourts.gov/AZSupremeCourt">
-                                        @ Arizona Supreme Court
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class = "experience-timeline">
-                        (June 2017 - August 2017)
-                    </div>
-                    <span class = "experience-content-break"/>
-                    <div class = "experience-content">
+                    <ExperienceCard experienceData = {ascExperienceData}>
                         <ul class = "experience-description">
                             <li class = "experience-list-item">
                                 Created a GUI viewing tool for querying a SQL database of court cases,
@@ -127,35 +91,11 @@ const Experience = () => {
                                 for the v 47.0 system update.
                             </li>
                         </ul>
-                    </div>
-                </div>
+                    </ExperienceCard>
 
+                    <br/>
 
-                <br/>
-
-
-                <div class = "experience-item">
-                    <div class = "title">
-                        <div class = "logo-container">
-                            <span class = "helper"/>
-                            <a rel = "noopener noreferrer" target = "_blank" href = "https://cidse.engineering.asu.edu/teaching-research-assistantships-fellowships-awards/"><img class = "logo" src = "img/asu-logo.png" alt = "Arizona State University Work Experience"/></a>
-                        </div>
-                        <div class = "outer-title-container">
-                            <div class = "inner-title-container">
-                                <div class = "experience-title">
-                                    Undergraduate Teaching Assistant {" "}
-                                    <a class = "external-links" target = "_blank" rel = "noopener noreferrer" href = "https://cidse.engineering.asu.edu/teaching-research-assistantships-fellowships-awards/">
-                                        @ Arizona State University
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class = "experience-timeline">
-                        (January 2017 - May 2017)
-                    </div>
-                    <span class = "experience-content-break"/>
-                    <div class = "experience-content">
+                    <ExperienceCard experienceData = {asuExperienceData}>
                         <ul class = "experience-description">
                             <li class = "experience-list-item">
                                 Improved student grades by an average of one letter grade relative
@@ -163,21 +103,21 @@ const Experience = () => {
                                 students outside lecture to aid in developing more intuitive
                                 understanding of object-oriented programming with Java.
                             </li>
-                            <li calss = "experience-list-item">
+                            <li class = "experience-list-item">
                                 Engaged student day-to-day comprehension through administering and guiding
                                 students on planned weekly in-class coding exercises.
                             </li>
                         </ul>
+                    </ExperienceCard>
+
+                    <div id = "signature">
+                        <a id = "signature-link" rel = "noopener noreferrer" target = "_blank" href = "https://github.com/rupponi"><h3>© 2019 Rohan Upponi</h3></a>
                     </div>
-                </div>
 
-                <div id = "signature">
-                <a id = "signature-link" rel = "noopener noreferrer" target = "_blank" href = "https://github.com/rupponi"><h3>© 2019 Rohan Upponi</h3></a>
                 </div>
-
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Experience;
